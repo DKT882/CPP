5c79a890616bdb70b2ba66cf3a7d564f44f6b1ec
dharmender kumar <107047333+DKT882@users.noreply.github.com>
2025-09-29 06:16:30 +0000

    inharitence

--- Tests/tst1.cpp
+++ Tests/tst1.cpp
@@ -5,31 +5,31 @@ int main(){
 
     // question 1st
     int Odd_Even;
-    cout<<"enter a no.";
+    cout<<"enter a no. :";
     cin>>Odd_Even;
     if (Odd_Even%2==0){
-        cout<<"Your no. is even";
+        cout<<"Your no. is even\n";
     }
     else{
-        cout<<"your no. is odd";
+        cout<<"your no. is odd\n";
     }
 
 
     // question 2nd >=90 a , > 75 b , >50 c, else fail
     int TotalMarks;
-    cout<<"enter Your marks";
+    cout<<"enter Your marks: ";
     cin>>TotalMarks;
     if (TotalMarks>=90){
-        cout<<"Grade A";
+        cout<<"Grade A\n";
     }
     else if (TotalMarks>=75){
-        cout<<"Grade B";
+        cout<<"Grade B\n";
     }
     else if (TotalMarks>=50){
-        cout<<"Grade C";
+        cout<<"Grade C\n";
     }
     else{
-        cout<<"you are fail";
+        cout<<"you are fail\n";
     }
 
     //question 3rd
@@ -40,31 +40,51 @@ int main(){
     switch (DayNumber)
     {
     case 1:
-        cout<<"Mon";
+        cout<<"Mon\n";
         break;
     case 2:
-        cout<<"Tues";
+        cout<<"Tues\n";
         break;
     case 3:
-        cout<<"Wed";
+        cout<<"Wed\n";
         break;
     case 4:
-        cout<<"Thu";
+        cout<<"Thu\n";
         break;
     case 5:
-        cout<<"Fri";
+        cout<<"Fri\n";
         break;
     case 6:
-        cout<<"Sat";
+        cout<<"Sat\n";
         break;
     case 7:
-        cout<<"Sun";
+        cout<<"Sun\n";
         break;
     default:
-        cout<<"Not a valid v";
+        cout<<"Not a valid day\n";
         break;
     }
 
 
+    // question 4th
+
+    for (int i=1;i <=10;i++){
+        cout<<5<<"X"<<i<<"="<<5*i<<"\n";
+    }
+
+    //question 5th
+    // using while loop
+    int k=1;
+    while (k<=5){
+        cout<<k<<endl;
+        k++;
+    }
+    //using do_wile loop
+    int doWhile=1;
+    do{
+        cout<<doWhile<<endl;
+        doWhile++;
+    }while(doWhile<=5);
+    
     return 0;
 }
