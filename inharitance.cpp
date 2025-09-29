bb587cba4c30b4e3f4dd5ee993f43b74d1b7bfb2
dharmender kumar <107047333+DKT882@users.noreply.github.com>
2025-09-29 06:30:03 +0000

    d

--- inharitance.cpp
+++ inharitance.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Animal {
-public:
-    void eat() {
-        cout << "This animal eats food." << endl;
-    }
-};
+// class Animal {
+// public:
+//     void eat() {
+//         cout << "This animal eats food." << endl;
+//     }
+// };
 
-class Dog : public Animal {
-public:
-    void bark() {
-        cout << "The dog barks." << endl;
-    }
-};
+// class Dog : public Animal {
+// public:
+//     void bark() {
+//         cout << "The dog barks." << endl;
+//     }
+// };
 
-int main() {
-    Dog myDog;
-    myDog.eat();
-    myDog.bark();
-    return 0;
-}
+// int main() {
+//     Dog myDog;
+//     myDog.eat();
+//     myDog.bark();
+//     return 0;
+// }
 
 class AnimalBase {
 public:
